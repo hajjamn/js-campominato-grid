539eaf4691e99e9dbef7613dd7966a632a50f2cf
hajjamn <hajjamn@gmail.com>
2024-03-13 16:16:05 +0100

    ADD: grid

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log('ciao mondo')
+const containerElement = document.querySelector('.game-container');
+let squareSideLenght = 10;
+
+for (let i = 0; i < Math.pow(squareSideLenght, 2); i++) {
+  const num = i + 1;
+
+  const cellElement = document.createElement('div');
+  cellElement.classList.add('cell')
+  cellElement.innerHTML = num
+
+  containerElement.append(cellElement)
+}
